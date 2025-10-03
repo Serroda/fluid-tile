ebaa5e96e41e0e033548188a49f9047976bb8ac4
serroda <jesuscarclem30@gmail.com>
2025-10-03 17:37:27 +0200

    regret: I cant use import or qt.include

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,8 +1,249 @@
-Qt.include("defaults.js");
-Qt.include("blocklist.js");
-Qt.include("windows.js");
-Qt.include("tiles.js");
-Qt.include("triggers.js");
+// DEFAULTS VARIABLES
+const APPS_BLOCKLIST =
+  "org.kde.kded6,qt-sudo,org.kde.polkit-kde-authentication-agent-1,org.kde.spectacle,kcm_kwinrules,org.freedesktop.impl.portal.desktop.kde,krunner,plasmashell,org.kde.plasmashell,kwin_wayland,ksmserver-logout-greeter";
+const MAXIMIZE_CLOSE = true;
+const MAXIMIZE_OPEN = true;
+const DESKTOP_ADD = true;
+const DESKTOP_REMOVE = true;
+const DESKTOP_REMOVE_DELAY = 300;
+const MODALS_IGNORE = true;
+const WINDOWS_ORDER_CLOSE = true;
+
+function checkBlocklist(windowItem, appsBlacklist, ignoreModals) {
+  return (
+    windowItem.normalWindow === false ||
+    windowItem.resizeable === false ||
+    windowItem.maximizable === false ||
+    (ignoreModals === true ? windowItem.transient === true : false) ||
+    appsBlacklist.includes(windowItem.resourceClass.toLowerCase()) === true
+  );
+}
+
+//TODO: REFACTOR
+//Get tiles, ordered by size (width) and from left to right
+function orderTiles(tiles) {
+  let tilesOrdered = [];
+
+  for (let tile of tiles) {
+    if (tile.tiles.length !== 0) {
+      tilesOrdered = tilesOrdered.concat(orderTiles(tile.tiles));
+    } else {
+      tilesOrdered.push(tile);
+    }
+  }
+
+  return tilesOrdered.sort((a, b) => {
+    if (b.absoluteGeometry.width !== a.absoluteGeometry.width) {
+      return b.absoluteGeometry.width - a.absoluteGeometry.width;
+    } else if (b.absoluteGeometry.height !== a.absoluteGeometry.height) {
+      return b.absoluteGeometry.height - a.absoluteGeometry.height;
+    } else {
+      return (
+        b.absoluteGeometry.x - a.absoluteGeometry.x &&
+        b.absoluteGeometry.y - a.absoluteGeometry.y
+      );
+    }
+  });
+}
+
+//Get tiles from the screen and virtual desktop
+function getOrderedTiles(desktop, screen) {
+  const rootTile = workspace.rootTile(screen, desktop);
+  return orderTiles(rootTile.tiles);
+}
+
+// Get all windows from the virtual desktop except the given window
+function getWindows(windowMain, desktop, screen, appsBlocklist, modalsIgnore) {
+  const windows = [];
+
+  for (const windowItem of workspace.windowList().reverse()) {
+    if (
+      windowItem !== windowMain &&
+      windowItem.output === screen &&
+      windowItem.desktops.includes(desktop) === true &&
+      checkBlocklist(windowMain, appsBlocklist, modalsIgnore) === false
+    ) {
+      windows.push(windowItem);
+    }
+  }
+
+  return windows;
+}
+
+// Set window tiles
+// mode: 0 => addWindow
+// mode: 1 => removeWindow
+function setWindowsTiles(
+  windowMain,
+  desktops,
+  screens,
+  appsBlocklist,
+  modalsIgnore,
+  maximize,
+  mode,
+  windowsOrderClose,
+) {
+  for (const itemDesktop of desktops) {
+    for (const itemScreen of screens) {
+      const windowsOther = getWindows(
+        windowMain,
+        itemDesktop,
+        itemScreen,
+        appsBlocklist,
+        modalsIgnore,
+      );
+
+      if (mode === 0) {
+        if (windowsOther.length === 0) {
+          workspace.currentDesktop = itemDesktop;
+          windowMain.desktops = [itemDesktop];
+
+          if (maximize === true) windowMain.setMaximize(true, true);
+          return false;
+        }
+      } else if (mode === 1) {
+        if (windowsOther.length === 1 && maximize === true) {
+          windowsOther[0].setMaximize(true, true);
+          return false;
+        }
+      }
+
+      if (mode === 1 && windowsOrderClose === false) {
+        return false;
+      }
+
+      const tilesOrdered = getOrderedTiles(itemDesktop, itemScreen);
+
+      if (mode === 0) {
+        //Set tile if the custom mosaic has space
+        if (windowsOther.length + 1 <= tilesOrdered.length) {
+          workspace.currentDesktop = itemDesktop;
+          windowMain.desktops = [itemDesktop];
+          windowMain.tile = tilesOrdered[0];
+          windowMain.setMaximize(false, false);
+          for (let x = 0; x < windowsOther.length; x++) {
+            windowsOther[x].desktops = [itemDesktop];
+            windowsOther[x].tile = tilesOrdered[x + 1];
+            windowsOther[x].setMaximize(false, false);
+          }
+          return false;
+        }
+      } else if (mode === 1 && windowsOther.length !== 0) {
+        for (let x = 0; x < windowsOther.length; x++) {
+          windowsOther[x].tile = tilesOrdered[x];
+          windowsOther[x].setMaximize(false, false);
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+//Delete Virtual Desktop if is empty or maximize the last window
+function onCloseWindow(windowClosed) {
+  const appsBlocklist = readConfig(
+    "AppsBlocklist",
+    APPS_BLOCKLIST,
+  ).toLowerCase();
+
+  const modalsIgnore = readConfig("ModalsIgnore", MODALS_IGNORE);
+
+  if (checkBlocklist(windowClosed, appsBlocklist, modalsIgnore) === true) {
+    return;
+  }
+
+  const maximizeClose = readConfig("CloseMaximize", MAXIMIZE_CLOSE);
+  const windowsOrderClose = readConfig(
+    "windowsOrderClose",
+    WINDOWS_ORDER_CLOSE,
+  );
+
+  const continueProcess = setWindowsTiles(
+    windowClosed,
+    windowClosed.desktops,
+    [windowClosed.output],
+    appsBlocklist,
+    modalsIgnore,
+    maximizeClose,
+    1,
+    windowsOrderClose,
+  );
+
+  if (continueProcess === false) {
+    return;
+  }
+
+  const desktopRemove = readConfig("DesktopRemove", DESKTOP_REMOVE);
+
+  if (desktopRemove === false) {
+    return;
+  }
+
+  const desktopRemoveDelay = readConfig(
+    "DesktopRemoveDelay",
+    DESKTOP_REMOVE_DELAY,
+  );
+
+  //Case: Applications that open a window and, when an action is performed,
+  //close the window and open another window (Chrome profile selector).
+  //This timer avoid crash wayland
+
+  const timer = new QTimer();
+  timer.singleShot = true;
+  timer.interval = desktopRemoveDelay;
+  timer.timeout.connect(function () {
+    for (const desktopItem of windowClosed.desktops) {
+      const windowsOtherSpecialCases = getWindows(
+        windowClosed,
+        desktopItem,
+        windowClosed.output,
+        appsBlocklist,
+        modalsIgnore,
+      );
+      if (windowsOtherSpecialCases.length === 0) {
+        workspace.removeDesktop(desktopItem);
+      }
+    }
+  });
+
+  timer.start();
+}
+
+//Set tile to the new Window
+function onOpenWindow(windowNew) {
+  const appsBlocklist = readConfig(
+    "AppsBlpcklist",
+    APPS_BLOCKLIST,
+  ).toLowerCase();
+  const modalsIgnore = readConfig("ModalsIgnore", MODALS_IGNORE);
+
+  if (checkBlocklist(windowNew, appsBlocklist, modalsIgnore) === true) {
+    return;
+  }
+
+  const maximizeOpen = readConfig("MaximizeOpen", MAXIMIZE_OPEN);
+  const desktopAdd = readConfig("DesktopAdd", DESKTOP_ADD);
+
+  const continueProcess = setWindowsTiles(
+    windowNew,
+    workspace.desktops,
+    workspace.screens,
+    appsBlocklist,
+    modalsIgnore,
+    maximizeOpen,
+    0,
+    false,
+  );
+
+  if (desktopAdd === true && continueProcess === true) {
+    workspace.createDesktop(workspace.desktops.length, "");
+    workspace.currentDesktop =
+      workspace.desktops[workspace.desktops.length - 1];
+    windowNew.desktops = [workspace.currentDesktop];
+    if (maximizeOpen === true) windowNew.setMaximize(true, true);
+  }
+}
 
 workspace.windowAdded.connect(onOpenWindow);
 workspace.windowRemoved.connect(onCloseWindow);
