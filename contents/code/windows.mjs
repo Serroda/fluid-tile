cf9184bd176a79eca9ddf4f48b1879a740c0d3e8
serroda <jesuscarclem30@gmail.com>
2025-12-23 11:06:40 +0100

    fix: dont maximize if the window is minimized

--- contents/code/windows.mjs
+++ contents/code/windows.mjs
@@ -101,7 +101,10 @@ export function useWindows(workspace, config) {
         } else if (mode === 1 && windowsOther.length !== 0) {
           if (maximize === true && windowsOther.length === 1) {
             windowsOther[0].tilePrevious = tilesOrdered[1];
-            windowsOther[0].setMaximize(true, true);
+
+            if (windowsOther[0].minimized === false) {
+              windowsOther[0].setMaximize(true, true);
+            }
           } else if (config.windowsOrderClose === true) {
             for (let x = 0; x < windowsOther.length; x++) {
               windowsOther[x].setMaximize(false, false);
