da8949c2fdd0a4948428111dd5fcde6ba87d25f7
serroda <jesuscarclem30@gmail.com>
2026-01-22 13:54:17 +0100

    feature[https://codeberg.org/Serroda/fluid-tile/issues/39]: change tile layout shortcut

--- contents/code/shortcuts.mjs
+++ contents/code/shortcuts.mjs
@@ -1,15 +1,28 @@
 export class Shortcuts {
-  constructor(workspace, root, { blocklist }) {
-    this.shortcuts = [
+  constructor(workspace, root, { blocklist, tiles }) {
+    this.layoutIndex = 0;
+    root.shortcuts = [
       {
         name: "FluidtileToggleWindowBlocklist",
-        text: "Fluid tile - Toggle window to blocklist",
+        text: "Fluid tile | Toggle window to blocklist",
         sequence: "Meta+F",
         callback: () => {
           blocklist.toggleWindow(workspace.activeWindow);
         },
       },
+      {
+        name: "FluidtileChangeTileLayout",
+        text: "Fluid tile | Change tile layout",
+        sequence: "Meta+Alt+F",
+        callback: () => {
+          const layouts = tiles.getDefaultLayouts();
+
+          tiles.setLayout(workspace.currentDesktop, layouts[this.layoutIndex]);
+
+          this.layoutIndex =
+            this.layoutIndex >= layouts.length - 1 ? 0 : this.layoutIndex + 1;
+        },
+      },
     ];
-    root.shortcuts = this.shortcuts;
   }
 }
